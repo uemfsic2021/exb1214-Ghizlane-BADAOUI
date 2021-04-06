9da8bc6bfe9fab8b977a8fad51740e8da1fdf1c2
GhizlaneBADAOUI <>
2021-04-06 21:26:49 +0000

    Done !

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,14 @@
 public class Palindrome{
-    public static boolean is_palindrome(String s)
-    {
-        // add your code here
-        return true;
+  public static boolean is_palindrome(String s) {
+    int i=0;
+    int j=s.length() - 1;
+
+    while ( (i<j) && (s.charAt(i)==s.charAt(j))) {
+      i=i+1; j=j-1;
+    }
+    if (s.charAt(i)!=s.charAt(j))
+      return false;
+    else
+      return true;
     }
 }
